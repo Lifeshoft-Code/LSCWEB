ef399b39fc8b722c38d1e34fde1da9e90ad4cbfa
Lifeshoft-Code <77507489+Lifeshoft-Code@users.noreply.github.com>
2021-02-18 17:09:43 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -11,6 +11,21 @@ btnCerrarForm.addEventListener('click', function(){
     ventanaForm.classList.add('active');
     
 });
+var btnAbrirMenu = document.getElementById('header-item_ico-menu');
+var btnCerrarMenu = document.getElementById('btn-close-menu');
+var menu = document.getElementById('main-menu');
+
+btnAbrirMenu.addEventListener('click', function(){
+    menu.classList.remove('close-menu');
+    // console.log("click");
+})
+btnCerrarMenu.addEventListener('click', function(){
+    menu.classList.add('close-menu');
+    
+    
+});
+//para que el usuario no pueda acceder al menu
+document.oncontextmenu = function(){return false}
 
 
 
